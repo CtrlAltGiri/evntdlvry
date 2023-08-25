2101a329b33fbf212a1b937f6cdd18dcf6378360
Giridhar Balachandran <giridharb@spitr.com>
2023-08-26 00:07:47 +0530

    Skeleton changes for all services Docker compose init

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Event Delivery Service
-Creating a sample pubsub using Go.
+Creating a sample pubsub using Python.
 
 ### Architecture
 Event -> Ingestion service -> Redis -> Delivery service -> Delivery endpoints.
+
+### Commands
+1. Launch all containers: `docker-compose up --build -d`
+2. Launch specific container again: `docker-compose up -d --no-deps --build <service name>`
---
+++ publisher/ingestion/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, make_response
+import json
+
+app = Flask(__name__)
+app.config.from_envvar('INGESTION_SETTINGS')
+
+@app.route("/event", methods=["POST"])
+def ingestEvent():
+    contentType = request.headers.get('Content-Type')
+    if contentType == 'application/json':
+        print(request.json)
+        return make_response('Accepted', 201)
+    else:
+        return make_response('Only JSON events are supported', 400)
+
+if __name__ == "__main__":
+    app.run(debug=True)
